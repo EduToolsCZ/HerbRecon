4a6309c4791dcc141926c591c58aa5f2f9d919d6
Sorashi <prazak.dennis@gmail.com>
2016-05-22 13:33:13 +0200

    Add Wikipedia API classes

---
+++ HerbReconListMaker/HerbReconListMakerTests/WikipediaApiUtilTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HerbReconListMaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerbReconListMaker.Tests
+{
+    [TestClass()]
+    public class WikipediaApiUtilTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            var result = WikipediaApiUtil.Search("Sedmikraska");
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual("Sedmikráska chudobka", result[0].Title);
+        }
+
+        [TestMethod()]
+        public void GetPageContentInJsonTest()
+        {
+            var result = WikipediaApiUtil.GetPageContentInJson("Sedmikráska chudobka");
+            var title = result["query"]["pages"]["306625"]["title"].ToString();
+            Assert.AreEqual("Sedmikráska chudobka", title);
+        }
+    }
+}
